aa607e62cb86170d56fc27aad9eec82f863d4b41
Douglas Rocha <douglasbellonrocha@gmail.com>
2015-08-29 16:40:52 -0300

    Made WebServices work

---
+++ StoneBackend/WebServices/Controllers/PostController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DomainModel.Entities;
+
+namespace WebServices.Controllers
+{
+    public class PostController : ApiController
+    {
+        // GET api/post
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/post/5
+        public Post Get(int id)
+        {
+            return new Post { Id = id, Title = "Titulo", Body = "Body", CreationDate = DateTime.Now, Author = null, Tags = null };
+        }
+
+        // POST api/post
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/post/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/post/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ StoneBackend/WebServices/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using DomainModel.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebServices.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public Post Get(int id)
+        {
+            return new Post { Title = "5", Body = "Lala", CreationDate = DateTime.Today, Id = 10, Author = null, Tags = null };
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
